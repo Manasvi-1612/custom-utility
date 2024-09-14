99826afdbab24b1e5bf18da2009360c108827e08
Manasvi-1612 <manasvijoshi63@gmail.com>
2024-09-14 19:19:06 +0530

    setting up button & select bx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,53 @@
-import { Select, SelectItem, SelectList, SelectTrigger } from "./components/select"
+import { Button } from "./components/button";
+import {
+  Select,
+  SelectItem,
+  SelectList,
+  SelectTrigger,
+} from "./components/select";
 
 function App() {
-
   return (
     <>
-      <Select className="mx-auto mt-10">
-        <SelectTrigger>Select an option</SelectTrigger>
-        <SelectList>
-          <SelectItem>Option 1</SelectItem>
-          <SelectItem>Option 2</SelectItem>
-          <SelectItem>Option 3</SelectItem>
-        </SelectList>
-      </Select>
-      <Select className="mx-auto mt-10" value="Option 1" onValueChange={(value) => console.log(value)}>
-        <SelectTrigger>Select an option</SelectTrigger>
-        <SelectList>
-          <SelectItem>Option 1</SelectItem>
-          <SelectItem>Option 2</SelectItem>
-          <SelectItem>Option 3</SelectItem>
-        </SelectList>
-      </Select>
+      <div className="w-max mx-auto mt-10">
+        <h1 className="text-lg font-semibold">Button</h1>
+        <div className="flex items-center gap-2">
+          <Button>Primary</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="ghost">Ghost</Button>
+          <Button variant="link">Link</Button>
+          <Button variant="outline">Outline</Button>
+          <Button size="small">Small</Button>
+          <Button size="icon">Icon</Button>
+        </div>
+      </div>
+
+      <div className="w-max mx-auto mt-10 ">
+        <h1 className="text-lg font-semibold">Select Box</h1>
+        <div className="flex gap-2">
+          <Select>
+            <SelectTrigger>Select an option</SelectTrigger>
+            <SelectList>
+              <SelectItem>Option 1</SelectItem>
+              <SelectItem>Option 2</SelectItem>
+              <SelectItem>Option 3</SelectItem>
+            </SelectList>
+          </Select>
+          <Select
+            value="Option 1"
+            onValueChange={(value) => console.log(value)}
+          >
+            <SelectTrigger>Select an option</SelectTrigger>
+            <SelectList>
+              <SelectItem>Option 1</SelectItem>
+              <SelectItem>Option 2</SelectItem>
+              <SelectItem>Option 3</SelectItem>
+            </SelectList>
+          </Select>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,164 +1,172 @@
-
 import { ChevronDownIcon } from "lucide-react";
 
 import { cn } from "../lib/cn";
 import {
-    createContext,
-    Dispatch,
-    FC,
-    ReactNode,
-    SetStateAction,
-    useContext,
-    useEffect,
-    useRef,
-    useState,
+  createContext,
+  Dispatch,
+  FC,
+  ReactNode,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
 } from "react";
 import { Button } from "./button";
 
 const base = "w-max relative";
 const baseT = "w-full px-3 justify-between";
 const baseL =
-    "absolute top-[110%] left-0 z-20 min-w-full w-max max-h-[60vh] h-max overflow-y-auto p-1 border border-zinc-200 rounded-md bg-white flex flex-col gap-1 scale-y-0 transition-transform duration-200 origin-top";
+  "absolute top-[110%] left-0 z-20 min-w-full w-max max-h-[60vh] h-max overflow-y-auto p-1 border border-zinc-200 rounded-md bg-white flex flex-col gap-1 scale-y-0 transition-transform duration-200 origin-top";
 const baseI = "w-full px-2 rounded";
 
 interface SelectContextPropsType {
-    isOpen: boolean;
-    setIsOpen: Dispatch<SetStateAction<boolean>>;
-    fallbackValue: string,
-    setFallbackValue: Dispatch<SetStateAction<string>>;
-    onValueChange?: (value: string) => void
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  fallbackValue: string;
+  setFallbackValue: Dispatch<SetStateAction<string>>;
+  onValueChange?: (value: string) => void;
 }
 
 const SelectContext = createContext<SelectContextPropsType | undefined>(
-    undefined
+  undefined
 );
 
 const useSelect = () => {
-    const context = useContext(SelectContext);
+  const context = useContext(SelectContext);
 
-    if (!context)
-        throw new Error("USE_SELECT must be used with in a SELECT_PROVIDER!");
+  if (!context)
+    throw new Error("USE_SELECT must be used with in a SELECT_PROVIDER!");
 
-    return context;
+  return context;
 };
 
 interface SelectPropsType {
-    children: ReactNode;
-    value?: string;
-    onValueChange?: (value: string) => void;
-    className?: string;
+  children: ReactNode;
+  value?: string;
+  onValueChange?: (value: string) => void;
+  className?: string;
 }
 
 const Select: FC<SelectPropsType> = ({
-    children,
-    value,
-    className,
-    onValueChange
+  children,
+  value,
+  className,
+  onValueChange,
 }) => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [fallbackValue, setFallbackValue] = useState<string>(value ?? '')
+  const [fallbackValue, setFallbackValue] = useState<string>(value ?? "");
 
-    const ref = useRef<HTMLDivElement | null>(null);
+  const ref = useRef<HTMLDivElement | null>(null);
 
-    useEffect(() => {
-        const handleClick = (event: any) => {
-            if (ref.current && !ref.current.contains(event.target)) {
-                setIsOpen(false);
-            }
-        };
+  useEffect(() => {
+    const handleClick = (event: any) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
 
-        document.addEventListener("mousedown", handleClick);
+    document.addEventListener("mousedown", handleClick);
 
-        return () => {
-            document.removeEventListener("mousedown", handleClick);
-        };
-    }, [ref]);
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, [ref]);
 
-    return (
-        <SelectContext.Provider value={{ isOpen, setIsOpen, fallbackValue, setFallbackValue, onValueChange }}>
-            <div ref={ref} className={cn(base, className ?? "")}>{children}</div>
-        </SelectContext.Provider>
-    );
+  return (
+    <SelectContext.Provider
+      value={{
+        isOpen,
+        setIsOpen,
+        fallbackValue,
+        setFallbackValue,
+        onValueChange,
+      }}
+    >
+      <div ref={ref} className={cn(base, className ?? "")}>
+        {children}
+      </div>
+    </SelectContext.Provider>
+  );
 };
 
 interface SelectTriggerPropsType {
-    children?: ReactNode;
-    className?: string;
+  children?: ReactNode;
+  className?: string;
 }
 
 const SelectTrigger: FC<SelectTriggerPropsType> = ({ children, className }) => {
-    const { fallbackValue, setIsOpen } = useSelect();
+  const { fallbackValue, setIsOpen } = useSelect();
 
-    const handleOnToggleIsOpen = () => setIsOpen((prev) => !prev);
+  const handleOnToggleIsOpen = () => setIsOpen((prev) => !prev);
 
-    return (
-        <Button
-            type="button"
-            variant="outline"
-            onClick={handleOnToggleIsOpen}
-            className={cn(baseT, className ?? "")}
-        >
-            {fallbackValue?.toString().trim() ? fallbackValue : children}
-            <ChevronDownIcon className="w-4 h-4" />
-        </Button>
-    );
+  return (
+    <Button
+      type="button"
+      variant="outline"
+      onClick={handleOnToggleIsOpen}
+      className={cn(baseT, className ?? "")}
+    >
+      {fallbackValue?.toString().trim() ? fallbackValue : children}
+      <ChevronDownIcon className="w-4 h-4" />
+    </Button>
+  );
 };
 
 interface SelectListPropsType {
-    children?: ReactNode;
-    className?: string;
+  children?: ReactNode;
+  className?: string;
 }
 
 const SelectList: FC<SelectListPropsType> = ({ children, className }) => {
-    const { isOpen } = useSelect();
+  const { isOpen } = useSelect();
 
-
-    return (
-        <div
-            className={cn(baseL, className ?? "", isOpen ? "scale-y-100" : "")}
-        >
-            {children}
-        </div>
-    );
+  return (
+    <div className={cn(baseL, className ?? "", isOpen ? "scale-y-100" : "")}>
+      {children}
+    </div>
+  );
 };
 
 interface SelectItemPropsType {
-    children?: ReactNode;
-    disabled?: boolean;
-    className?: string;
+  children?: ReactNode;
+  disabled?: boolean;
+  className?: string;
 }
 
 const SelectItem: FC<SelectItemPropsType> = ({
-    children,
-    disabled,
-    className,
+  children,
+  disabled,
+  className,
 }) => {
-    const { fallbackValue, setIsOpen, setFallbackValue, onValueChange } = useSelect();
+  const { fallbackValue, setIsOpen, setFallbackValue, onValueChange } =
+    useSelect();
 
-    const handleSelectOption = () => {
+  const handleSelectOption = () => {
+    setFallbackValue(children ? children?.toString() : "");
 
-        setFallbackValue(children ? children?.toString() : "");
+    onValueChange && onValueChange(children?.toString() ?? "");
 
-        onValueChange && onValueChange(children?.toString() ?? '')
+    setIsOpen(false);
+  };
 
-        setIsOpen(false);
-    };
-
-    return (
-        <Button
-            type="button"
-            variant={
-                children && children.toString() === fallbackValue ? "secondary" : "ghost"
-            }
-            size="small"
-            onClick={handleSelectOption}
-            disabled={disabled}
-            className={cn(baseI, className ?? "")}
-        >
-            {children}
-        </Button>
-    );
+  return (
+    <Button
+      type="button"
+      variant={
+        children && children.toString() === fallbackValue
+          ? "secondary"
+          : "ghost"
+      }
+      size="small"
+      onClick={handleSelectOption}
+      disabled={disabled}
+      className={cn(baseI, className ?? "")}
+    >
+      {children}
+    </Button>
+  );
 };
 
 export { Select, SelectTrigger, SelectList, SelectItem };
